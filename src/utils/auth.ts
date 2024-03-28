f13ab1d09336599d194338f4d5ae936cab611284
SantiZai <santyagozaidan@gmail.com>
2024-03-28 20:36:08 -0300

    login service

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,19 +2,20 @@ import envVars from "../../env";
 import { Employee } from "./models";
 
 export const login = async (employee: Partial<Employee>) => {
+  const credentials = {
+    fullname: employee.fullname.toLowerCase().trim(),
+    identifier: employee.identifier.toString().trim(),
+  };
   const config = {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      fullname: employee.fullname.trim(),
-      identifier: employee.identifier.toString().trim(),
-    }),
+    body: JSON.stringify(credentials),
   };
   try {
     const result = await fetch(`${envVars.API_BASE}/auth/login`, config);
-    return result.json()
+    return result.json();
   } catch (e) {
     console.error(e);
   }
