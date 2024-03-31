f7363050cec981a857c9a426516671a242c18deb
SantiZai <santyagozaidan@gmail.com>
2024-03-31 15:59:28 -0300

    pressable card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,29 @@
-import { View } from "react-native";
+import { Pressable, View } from "react-native";
 import CustomText from "./CustomText";
 import { mainStyles } from "../mainStyles.module";
 
-
-const Card = ({ name, type }: { name: string; type: string }) => {
+const Card = ({
+  name,
+  type,
+  onPress,
+}: {
+  name: string;
+  type: string;
+  onPress?: () => void;
+}) => {
   return (
     <View style={mainStyles.card}>
-      <CustomText styles={mainStyles.cardTitle}>{name}</CustomText>
-      <CustomText styles={mainStyles.paragraph}>{type}</CustomText>
+      {onPress ? (
+        <Pressable onPress={onPress}>
+          <CustomText styles={mainStyles.cardTitle}>{name}</CustomText>
+          <CustomText styles={mainStyles.paragraph}>{type}</CustomText>
+        </Pressable>
+      ) : (
+        <>
+          <CustomText styles={mainStyles.cardTitle}>{name}</CustomText>
+          <CustomText styles={mainStyles.paragraph}>{type}</CustomText>
+        </>
+      )}
     </View>
   );
 };
