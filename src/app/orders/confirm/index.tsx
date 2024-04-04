da7e20e784b7e6988be9de05ec509a11a9b3d7c3
SantiZai <santyagozaidan@gmail.com>
2024-04-04 15:14:56 -0300

    confirm order page

--- src/app/orders/confirm/index.tsx
+++ src/app/orders/confirm/index.tsx
@@ -1,1 +1,13 @@
-//TODO
+import CenterView from "../../../components/CenterView";
+import CustomText from "../../../components/CustomText";
+
+const ConfirmPage = () => {
+  return (
+    <CenterView>
+      <CustomText title>¡Orden creada exitosamente!</CustomText>
+      <CustomText>Su pedido ya está en nuestro registro y pronto se le avisará para poder retirarlo.</CustomText>
+    </CenterView>
+  );
+};
+
+export default ConfirmPage;
